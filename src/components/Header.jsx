3c493f46ffcc3952eaa51dc12d84e6fdfc4d38ac
divvsaxena <saxenadivv@gmail.com>
2024-09-28 20:18:08 +0530

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,9 @@ const Header = () => {
         <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             Connect Wallet
         </button>
-        
+        <button className="inline-flex h-12 animate-shimmer mr-6 items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            Profile Badge
+        </button>
         
     </div>
   )
